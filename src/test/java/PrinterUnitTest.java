970f543cfba4dabfe67d5e09d1e90fe73be9609c
Retro <disbkz@gmail.com>
2023-11-22 14:22:46 +0600

    Fixing the tests part II

---
+++ src/test/java/PrinterUnitTest.java
@@ -0,0 +1,28 @@
+import org.example.Printer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+class PrinterUnitTest {
+
+    private BlockingDeque<Integer> stack;
+    private Printer printer;
+
+    @BeforeEach
+    void setUp() {
+        stack = new LinkedBlockingDeque<>(List.of(1, 2, 3, 4, 5));
+        printer = new Printer(stack);
+    }
+
+    @Test
+    void run() throws InterruptedException {
+
+        printer.start();
+        Thread.sleep((long) Printer.PRINTER_TIME_MAX * stack.size() + 1000);
+        printer.interrupt();
+
+    }
+}
--- src/test/java/unit_test/NumberGeneratorUnitTest.java
+++ src/test/java/unit_test/NumberGeneratorUnitTest.java
@@ -1,55 +1,82 @@
-//This Class under modification
+package unit_test;
 
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.stream.Stream;
 
-//package unit_test;
-//
-//import java.util.concurrent.BlockingDeque;
-//import java.util.concurrent.LinkedBlockingDeque;
-//
-//import org.example.NumberGenerator;
+import org.example.NumberGenerator;
 //import org.junit.Before;
 //import org.junit.Test;
-//
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
+
 //import static org.junit.Assert.*;
-//
-//public class NumberGeneratorUnitTest {
-//
-//    private BlockingDeque<Integer> sharedStack;
-//    private NumberGenerator numberGenerator;
-//
-//    @Before
-//    public void setUp() {
-//
-//        sharedStack = new LinkedBlockingDeque<>(50);
-//        numberGenerator = new NumberGenerator(sharedStack, 3, 3);
-//    }
-//
-//    @Test
-//    public void testGetNotNull() {
-//        assertNotNull(numberGenerator.get());
-//    }
-//
-//    @Test
-//    public void testGetCheckValue() {
-//        int result = numberGenerator.get();
-//        assertEquals(3, result);
-//    }
-//
-//
-//
-//
-//    @Test
-//    public void testRun() throws InterruptedException {
-//
-//        numberGenerator.start();
-//
-//        Thread.sleep(10000);
-//        numberGenerator.interrupt();
-//
-//        while (!sharedStack.isEmpty()) {
-//            Integer poppedElement = sharedStack.pop();
-//            assert poppedElement % numberGenerator.getStep() == 0 : "Assertion failed: condition not met";
-//        }
-//    }
-//}
-//
+
+public class NumberGeneratorUnitTest {
+
+    private static BlockingDeque<Integer> sharedStack;
+    private static NumberGenerator numberGenerator;
+    private static boolean isParameterizedTest = false;
+
+
+    @BeforeAll
+    public static void setUpBeforeAll() {
+
+        if (isParameterizedTest) {
+            sharedStack = new LinkedBlockingDeque<>(50);
+            numberGenerator = new NumberGenerator(sharedStack, 3, 3);
+        }
+    }
+
+    @BeforeEach
+    public void setUp() {
+
+        if (!isParameterizedTest) {
+            sharedStack = new LinkedBlockingDeque<>(50);
+            numberGenerator = new NumberGenerator(sharedStack, 3, 3);
+        }
+    }
+
+    @Test
+    public void testGetNotNull() {
+
+        assertNotNull(numberGenerator.get());
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("expectedValuesProvider")
+    public void testGetCheckValueWithParameter(int expectedValue) {
+
+        isParameterizedTest = true;
+        int result = numberGenerator.get();
+        assertEquals(expectedValue, result);
+    }
+
+    private static Stream<Integer> expectedValuesProvider() {
+        // Provide the expected values in the sequence
+        return Stream.of(3, 6, 9, 12, 15, 18, 21, 24, 27, 30);
+    }
+
+
+
+    @Test
+    public void testRun() throws InterruptedException {
+
+        numberGenerator.start();
+
+        Thread.sleep(NumberGenerator.GENERATOR_TIME_MAX * 5);
+        numberGenerator.interrupt();
+
+        while (!sharedStack.isEmpty()) {
+            Integer poppedElement = sharedStack.pop();
+            assert poppedElement % numberGenerator.getStep() == 0 : "Assertion failed: One or more elements are not divisible by %s".formatted(numberGenerator.getStep());
+        }
+    }
+}
+
